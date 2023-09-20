c0f946fe760d0a21447c1e5a444a8ddc5de7c9b6
Reydner96 {Reyd} <31974666+Reydner96@users.noreply.github.com>
2023-09-20 15:30:04 -0300

    Update

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class DadosCadastraisPage extends StatefulWidget {
-  const DadosCadastraisPage({super.key});
+  const DadosCadastraisPage({Key? key}) : super(key: key);
 
   @override
   State<DadosCadastraisPage> createState() => _DadosCadastraisPageState();
 }
 
 class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
+  var nomeController = TextEditingController(text: "");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text("Dados Cadastráis"),
+      appBar: AppBar(
+        title: const Text("Meus Dados"),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Nome",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+            ),
+            TextField(
+              controller: nomeController,
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint(nomeController.text);
+              },
+              child: Text("Salvar"),
+            )
+          ],
+        ),
       ),
     );
   }
